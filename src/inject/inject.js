cfc7d75e7a40245c9cf86dfd61700b2d652af05d
Dominic Bartl <dominic.bartl.at@gmail.com>
2014-09-02 20:07:27 +0200

    Requestion bookmarks in background page

--- src/inject/inject.js
+++ src/inject/inject.js
@@ -1,13 +1,4 @@
-chrome.extension.sendMessage({}, function(response) {
-	var readyStateCheckInterval = setInterval(function() {
-	if (document.readyState === "complete") {
-		clearInterval(readyStateCheckInterval);
-
-		// ----------------------------------------------------------
-		// This part of the script triggers when page is done loading
-		console.log("Hello. This message was sent from scripts/inject.js");
-		// ----------------------------------------------------------
-
-	}
-	}, 10);
-});
+chrome.runtime.onMessage.addListener(
+	function(request, sender, sendResponse) {
+		console.log(request);
+	});
